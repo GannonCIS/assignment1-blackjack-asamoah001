e832c4850c2f6fbaed39ccf0335a3b912d608182
Paul Gyasi Asamoah <asamoah001@gannon.edu>
2020-02-03 15:59:43 -0500

    Implemented init deck and added printeddeck for testing

--- src/blackjack/Deck.java
+++ src/blackjack/Deck.java
@@ -11,15 +11,22 @@ package blackjack;
  */
 public class Deck {
     private Card[] myCards = new Card[52];
-    private String[] ranks = {};
-    private String[] suits ={};
+    private String[] ranks = {"Ace","2","3","4","5","6","7","8","9","10","Jack","Queen","King"};
+    private String[] suits ={"clubs","Diamonds","Haearts","Spades"};
     private int nextCard = 0;
     
     public Deck(){
-        
+        initDeck();
     }
         
     private void initDeck(){
+        int index = 0;
+        for(int s =0; s < suits.length; s++){
+            for(int r = 0; r<ranks.length; r++){
+                myCards[index] = new Card(ranks[r], suits[s]);
+                index++; 
+            }
+        }
     
     }
     
@@ -28,8 +35,15 @@ public class Deck {
     }
     // printDeck is for testing only --delete me
     public void printDeck(){
-    
+        for(int i=0;i < myCards.length; i++){
+            System.out.println(myCards[i].RANK + "of"+ myCards[i].SUIT);
+        }
     }
+        
+    /**
+     *
+     * @return
+     */
     public Card dealCard(){
     
         return null; //do no forget to change !!!!!!!!!!
